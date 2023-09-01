aeece8735dec33717e350b85fd6c47721621397f
jeremymtan <jemtan@ucsc.edu>
2023-09-01 09:05:26 -0400

    added test cases for pytest

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-# taken from https://pytest.org/en/7.4.x/getting-started.html#run-multiple-tests
-def func(x):
-    return x + 1
+# file from another assignment
+def listMax(list):
+    if not list:
+        return None
+    max_num = -999999999999
+    for i in list:
+        if i > max_num:
+            max_num = i
+
+    return max_num
 
 
-def test_answer():
-    assert func(3) == 4
+def doTest(list):
+    if list == None:
+        return None
+        pass
+    else:
+        n = len(list)
+        for i in range(0, n):
+            if i < n - 1:
+                pass
+            pass
+        pass
+    max = listMax(list)
+    if max == None:
+        return None
+        pass
+    else:
+        return max
+        pass
+    pass
+
+
+def main():
+    list1 = [77, 33, 19, 99, 42, 6, 27, 4]
+    list2 = [-3, -42, -99, -1000, -999, -88, -77]
+    list3 = [425, 59, -3, 77, 0, 36]
+    assert doTest(list1) == 99
+    assert doTest(list2) == -3
+    assert doTest(list3) == 425
+
+
+if __name__ == "__main__":
+    main()
+    pass
