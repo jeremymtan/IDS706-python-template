0395407ca3f0f64416e1afcc7b7991b0c5f9475e
jeremymtan <jemtan@ucsc.edu>
2023-09-01 09:15:56 -0400

    added test cases for pytest v3

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-# file from another assignment
-def listMax(list):
-    if not list:
+def listmax(list_max):
+    """Function returning max of list"""
+    if not list_max:
         return None
     max_num = -999999999999
-    for i in list:
+    for i in list_max:
         if i > max_num:
             max_num = i
 
     return max_num
 
 
-def doTest(list):
-    max = listMax(list)
-    if max == None:
-        return None
-    else:
-        return max
-
-
 def main():
+    """Function calling listMax"""
+
     list1 = [77, 33, 19, 99, 42, 6, 27, 4]
     list2 = [-3, -42, -99, -1000, -999, -88, -77]
     list3 = [425, 59, -3, 77, 0, 36]
-    assert doTest(list1) == 99
-    assert doTest(list2) == -3
-    assert doTest(list3) == 425
+    assert listmax(list1) == 99
+    assert listmax(list2) == -3
+    assert listmax(list3) == 425
 
 
 if __name__ == "__main__":
